9aa5adea69a1fb72aae9a80942b1e37a90c097bb
Harry-Drew-Uni <91594213+Harry-Drew-Uni@users.noreply.github.com>
2022-02-11 17:54:30 +0000

    Mini Bank Account - Started

---
+++ Mini Bank Account/Mini Bank Account/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mini_Bank_Account
+{
+    class BankAccount
+    {
+        public string AccountName { get; private set; }
+        public int AccountNumber { get; private set; }
+        public decimal Balance { get; private set; }
+
+        //Declaring bank account
+        public BankAccount(string _customerName, int _accountNumber, decimal _balance) 
+        {
+            AccountName = _customerName;
+            AccountNumber = _accountNumber;
+            Balance = _balance;
+        }
+
+        //Initializing bank account with no balance
+        public BankAccount(string _customerName, int _accountNumber) : this(_customerName, _accountNumber, 0) { }
+
+        public void AddFunds(decimal _amount) 
+        {
+            Balance += _amount;
+        }
+
+        public void WithdrawFunds(decimal _amount) 
+        {
+            Balance -= _amount;
+        }
+    
+    }
+}
---
+++ Mini Bank Account/Mini Bank Account/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Mini_Bank_Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random();
+            int accountNumber = generator.Next(0, int.MaxValue);
+
+            Console.WriteLine("=========================");
+            Console.WriteLine("CREATE BANK ACCOUNT");
+            Console.WriteLine("=========================");
+            Console.WriteLine("Enter account name: ");
+            string accountName = Console.ReadLine();
+            Console.WriteLine("Enter starting balance: ");
+            decimal startingBalance = decimal.Parse(Console.ReadLine());
+            BankAccount account = new BankAccount(accountName, accountNumber, startingBalance);
+
+            Console.WriteLine("What do you want to do:");
+            Console.WriteLine("1. Withdraw");
+            Console.WriteLine("2. Deposit");
+            Console.WriteLine("3. Exit");
+
+            int userInput = int.Parse(Console.ReadLine());
+            if (userInput == 1)
+            {
+                Console.WriteLine("Enter the amount you want to withdraw: ");
+                decimal amount = decimal.Parse(Console.ReadLine());
+                account.WithdrawFunds(amount);
+                Console.WriteLine("Sucessfully withdrawn {0}", amount);
+            }
+            else if (userInput == 2)
+            {
+                Console.WriteLine("Enter the amount you want to deposit: ");
+                decimal amount = decimal.Parse(Console.ReadLine());
+                account.AddFunds(amount);
+                Console.WriteLine("Sucessfully added funds of {0}", amount);
+            }
+            else 
+            {
+                Console.WriteLine("Exited: You have sucessfully exited the application");
+            }
+
+        }
+    }
+}
